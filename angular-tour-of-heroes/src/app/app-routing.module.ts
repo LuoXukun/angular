40cd529de5fc048e1f16bccec571a0d3f5182904
Luo XuKun <861392914@qq.com>
2018-09-27 15:04:13 +0800

    路由

--- angular-tour-of-heroes/src/app/app-routing.module.ts
+++ angular-tour-of-heroes/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { HeroesComponent } from './heroes/heroes.component';
+
+// 添加路由定义
+const routes: Routes = [
+  {path: 'heroes', component: HeroesComponent}
+];
+
+
+// 要在后面？
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]   //初始化路由器，并监听浏览器中的地址变化
 })
+
 export class AppRoutingModule { }
