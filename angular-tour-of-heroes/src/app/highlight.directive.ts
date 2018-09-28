4d2ccb5149ab69d546e1c03c0a4ec9db4f9290a7
Luo XuKun <861392914@qq.com>
2018-09-28 17:39:02 +0800

    the directive

--- angular-tour-of-heroes/src/app/highlight.directive.ts
+++ angular-tour-of-heroes/src/app/highlight.directive.ts
@@ -1,12 +1,24 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'yellow';
+  @Input('appHighlight') highlightColor: string;
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onmouseenter() {
+    this.highlight(this.highlightColor || 'red');
+  }
+
+  @HostListener('mouseleave') onmouseleave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
   }
 
 }
