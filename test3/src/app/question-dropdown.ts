61c983c7753189e6e364fe3b67c864567a955290
Luo XuKun <861392914@qq.com>
2018-10-08 11:24:08 +0800

    下拉框类

---
+++ test3/src/app/question-dropdown.ts
@@ -0,0 +1,10 @@
+import { QuestionBase } from "./question-base";
+
+export class QuestionDropdown extends QuestionBase<string> {
+    controlType = 'dropdown';
+    options: {key: string, value: string}[] = [];
+    constructor(options: {} = {}) {
+        super(options);
+        this.options = options['options'] || [];
+    }
+}
