050545e78eada6d4dd72106f750bd251c72887a5
Luo XuKun <861392914@qq.com>
2018-10-08 16:17:18 +0800

    debug full pathname

--- customer-app/src/app/app-routing.module.ts
+++ customer-app/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'customers', loadChildren: 'app/customers/customers.module#CustomersModule'},
-  {path: 'orders', loadChildren: 'app/orders/orders.module#OrdersModule'},
+  {path: 'customers', loadChildren: 'src/app/customers/customers.module#CustomersModule'},
+  {path: 'orders', loadChildren: 'src/app/orders/orders.module#OrdersModule'},
   {path: '', redirectTo: '', pathMatch: 'full'}
 ];
 
