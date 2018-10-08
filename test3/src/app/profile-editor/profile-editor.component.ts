b77e15f0de393f60bb1b990ed93f2adb0ddcc752
Luo XuKun <861392914@qq.com>
2018-10-08 10:00:50 +0800

    嵌套的表单组

--- test3/src/app/profile-editor/profile-editor.component.ts
+++ test3/src/app/profile-editor/profile-editor.component.ts
@@ -10,7 +10,13 @@ export class ProfileEditorComponent implements OnInit {
 
   profileForm = new FormGroup({
     firstName: new FormControl(''),
-    lastName: new FormControl('')
+    lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
   });
 
   constructor() { }
