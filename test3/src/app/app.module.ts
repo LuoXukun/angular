0001b73fd183749cf7060eda984fdc650d284102
Luo XuKun <861392914@qq.com>
2018-10-08 11:40:18 +0800

    问卷表单容器及入口点

--- test3/src/app/app.module.ts
+++ test3/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { NameEditorComponent } from './name-editor/name-editor.component';
 import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameEditorComponent,
-    ProfileEditorComponent
+    ProfileEditorComponent,
+    DynamicFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ test3/src/app/dynamic-form/dynamic-form.component.ts
@@ -0,0 +1,29 @@
+/* 问卷表单容器及入口点 */
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from "@angular/forms";
+
+import { QuestionBase } from "../question-base";
+import { QuestionControlService } from "../question-control.service";
+
+@Component({
+  selector: 'app-dynamic-form',
+  templateUrl: './dynamic-form.component.html',
+  styleUrls: ['./dynamic-form.component.css']
+})
+export class DynamicFormComponent implements OnInit {
+
+  @Input() questions: QuestionBase<any>[] = [];
+  form: FormGroup;
+  payLoad = '';
+
+  constructor(private qcs: QuestionControlService) { }
+
+  ngOnInit() {
+    this.form = this.qcs.toFromGroup(this.questions);
+  }
+
+  onSubmit() {
+    this.payLoad = JSON.stringify(this.form.value);
+  }
+
+}
